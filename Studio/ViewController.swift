5466838e9013ea1dc35b370ee514c45f21fca368
banjun <banjun@gmail.com>
2018-06-17 17:25:27 +0900

    track face by eyes & nose

--- Studio/ViewController.swift
+++ Studio/ViewController.swift
@@ -3,6 +3,8 @@ import AVFoundation
 import Vision
 
 private let poseNet = PoseNet()
+private let modelSize = CGSize(width: 337, height: 337) // see posenet337.mlmodel input
+private let referenceEyeDistance: CGFloat = 320
 
 class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
     var cameraSession: AVCaptureSession?
@@ -12,13 +14,16 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
             oldValue?.removeFromSuperlayer()
             guard let newValue = previewLayer else { return }
             newValue.frame = view.layer.bounds
-            view.layer.addSublayer(newValue)
+            view.layer.insertSublayer(newValue, at: 0)
         }
     }
+
+    let faceImageView = UIImageView(image: UIImage(named: "face")!)
 
     let poseNetModel = try! VNCoreMLModel(for: posenet337().model)
     private lazy var poseNetRequest: VNCoreMLRequest = {
         let req = VNCoreMLRequest(model: poseNetModel) { request, error in
+            guard let orientation = self.cameraSession?.outputs.first?.connections.first?.videoOrientation else { return }
             guard let obs = request.results as? [VNCoreMLFeatureValueObservation] else { return }
             let values = obs.compactMap {$0.featureValue.multiArrayValue}
             guard values.count >= 4 else { return }
@@ -38,15 +43,52 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
 //                displacement_fwd_2__0: values[2],
 //                displacement_bwd_2__0: values[3])
             DispatchQueue.main.async {
-                self.poses = poses
+                self.posesWithOrientation = (poses, orientation)
             }
         }
         req.imageCropAndScaleOption = .scaleFill
         return req
     }()
-    var poses: [Pose] = [] {
+    var posesWithOrientation: (poses: [Pose], orientation: AVCaptureVideoOrientation) = ([], .landscapeLeft) {
         didSet {
-            NSLog("%@", "prediction: \(String(describing: poses))")
+            let (poses, orientation) = posesWithOrientation
+//            NSLog("%@", "prediction: \(String(describing: poses))")
+            faceImageView.isHidden = poses.isEmpty
+            guard let pose = poses.first else { return }
+            guard let leftEye = (pose.keypoints.first {$0.part == "leftEye"}) else { return }
+            guard let rightEye = (pose.keypoints.first {$0.part == "rightEye"}) else { return }
+            guard let nose = (pose.keypoints.first {$0.part == "nose"}) else { return }
+            let eyeDistance = CGFloat(sqrt(pow(leftEye.position.x - rightEye.position.x, 2) +
+                pow(leftEye.position.y - rightEye.position.y, 2)))
+
+            let rotation: CGFloat = {
+                switch orientation {
+                case .landscapeLeft: return -.pi / 2
+                case .portrait: return 0
+                case .landscapeRight: return .pi / 2
+                case .portraitUpsideDown: return .pi
+                }
+            }()
+
+            let transform = CGAffineTransform.identity
+                .scaledBy(x: view.bounds.width, y: view.bounds.height)
+                .translatedBy(x: +0.5, y: +0.5)
+                .scaledBy(x: 1, y: -1)
+                .rotated(by: rotation)
+                .translatedBy(x: -0.5, y: -0.5)
+                .scaledBy(x: 1 / modelSize.width, y: 1 / modelSize.height)
+
+            let estimatedScale = (eyeDistance / min(modelSize.width, modelSize.height))
+                * (min(faceImageView.intrinsicContentSize.width,
+                       faceImageView.intrinsicContentSize.height) / referenceEyeDistance)
+
+            UIView.animate(withDuration: 0.1) {
+                self.faceImageView.center = CGPoint(
+                    x: CGFloat(nose.position.x),
+                    y: CGFloat(nose.position.y))
+                    .applying(transform)
+                self.faceImageView.transform = CGAffineTransform(scaleX: estimatedScale, y: estimatedScale)
+            }
         }
     }
 
@@ -54,6 +96,12 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         super.init(nibName: nil, bundle: nil)
     }
     required init?(coder aDecoder: NSCoder) {fatalError()}
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.addSubview(faceImageView)
+    }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -99,8 +147,6 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         } catch {
             NSLog("%@", "handler.perform throws: \(error)")
         }
-//
-//        let prediction = try? poseNet.prediction(image__0: pixelBuffer)
     }
 }
 
